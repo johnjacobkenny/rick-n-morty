c016971bece020910fecc0c5d1db7fb8fd82dffa
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-01-19 15:40:04 +0530

    Build

---
+++ docs/build/precache-manifest.7a37ddefffbe4903ec1885921fef582f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c5701debe266f34f8118408077a633",
+    "url": "/rick-n-morty/index.html"
+  },
+  {
+    "revision": "719678114a1ba8049eb7",
+    "url": "/rick-n-morty/static/css/main.7238c92c.chunk.css"
+  },
+  {
+    "revision": "7495f0456e33b3d800cf",
+    "url": "/rick-n-morty/static/js/2.13487666.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rick-n-morty/static/js/2.13487666.chunk.js.LICENSE"
+  },
+  {
+    "revision": "719678114a1ba8049eb7",
+    "url": "/rick-n-morty/static/js/main.776f0c2b.chunk.js"
+  },
+  {
+    "revision": "56e953a55d770ed21b0e",
+    "url": "/rick-n-morty/static/js/runtime-main.4d53ad1e.js"
+  },
+  {
+    "revision": "5a59177ef7138d50b3f5e3eb5b86b34a",
+    "url": "/rick-n-morty/static/media/rnm.5a59177e.png"
+  }
+]);
