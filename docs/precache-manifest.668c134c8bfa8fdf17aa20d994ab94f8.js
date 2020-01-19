c24b1549aef4771bfb572d0f041e5435d14683c3
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-01-19 14:35:00 +0530

    Generate build

---
+++ docs/precache-manifest.668c134c8bfa8fdf17aa20d994ab94f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c13eb79f4208ee97f07fc1fccf6349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1efa8904cb44099aca46",
+    "url": "/static/css/main.96cb6289.chunk.css"
+  },
+  {
+    "revision": "822d794503f80a1ef905",
+    "url": "/static/js/2.852d020c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.852d020c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1efa8904cb44099aca46",
+    "url": "/static/js/main.961eeef7.chunk.js"
+  },
+  {
+    "revision": "638e87afac6d7aa22380",
+    "url": "/static/js/runtime-main.08c91656.js"
+  }
+]);
